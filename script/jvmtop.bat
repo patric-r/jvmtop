3546c5d4e53ab4add4a8f51bf59dc2ac8562f9a1
Franco Lombardo <f.lombardo69@gmail.com>
2016-01-19 08:52:36 +0100

    Standard Maven structure

--- script/jvmtop.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set DIR=%~dp0
-set JAVA_HOME="c:\Program Files\Java\jdk1.8.0_25"
-cls
-
-%JAVA_HOME%\bin\java %JAVA_OPTS% -cp %DIR%/jvmtop.jar;%JAVA_HOME%/lib/tools.jar com.jvmtop.JvmTop %* 
