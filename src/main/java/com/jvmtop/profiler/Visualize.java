517ca298510aa0e69a78dbed74bfc0ea99f11e74
Grigory Turunov <g.turunov@corp.mail.ru>
2017-07-18 18:49:38 +0300

    Tunings

--- src/main/java/com/jvmtop/profiler/Visualize.java
+++ src/main/java/com/jvmtop/profiler/Visualize.java
@@ -5,11 +5,11 @@ import java.util.List;
 
 public class Visualize {
     private static final int PADDING = 4;
-    private static final int OUT_FORMAT_LEN = 1 + 5 + 3 + 5 + 5 + 1; // [(99.0% | 88.0% self)]
-    private static final int TOTAL_FORMAT_LEN = 1+ 1 + 5 + 1 + 5 + 1; // [ (99.0% total)]
+    private static final int OUT_FORMAT_LEN = 20; // [(99.0% | 88.0% self)]
+    private static final int TOTAL_FORMAT_LEN = 30; // [ (99.0% total | 88.0% process)]
     private static final String BRANCH = " \\_ ";
 
-    public static void print(CalltreeNode node, long parentTotalTime, long threadTotalTime, PrintStream out, int depth, Config config, boolean skipped) {
+    public static void print(CalltreeNode node, long parentTotalTime, long threadTotalTime, long processTotalTime, PrintStream out, int depth, Config config, boolean skipped) {
         if (depth > config.maxDepth) return;
         double percentFull = node.getTotalTime() * 100.0 / parentTotalTime;
         double percentSelf = node.getSelf() * 100.0 / parentTotalTime;
@@ -25,7 +25,7 @@ public class Visualize {
         if (config.printTotal) functionMaxWidth -= TOTAL_FORMAT_LEN;
         if (functionMaxWidth <= 0) return;
 
-        List<CalltreeNode> children = node.getSortedChildren(config.minCost, parentTotalTime);
+        List<CalltreeNode> children = node.getSortedChildren(config.minCost, threadTotalTime);
 
         boolean skipping = config.canSkip && node.getTotalTime() == parentTotalTime && children.size() == 1 && node.getSelf() == 0 && depth > 0;
         if (skipping) {
@@ -38,8 +38,9 @@ public class Visualize {
             out.printf("%." + functionMaxWidth + "s (%.1f%% | %.1f%% self)",
                     node.getName(), percentFull, percentSelf, node.getIntermediate(), node.getSelf());
             if (config.printTotal) {
-                double percentTotal = node.getTotalTime() * 100.0 / threadTotalTime;
-                out.printf(" (%.1f%% total)\n", percentTotal);
+                double percentThread = node.getTotalTime() * 100.0 / threadTotalTime;
+                double percentProcess = node.getTotalTime() * 100.0 / processTotalTime;
+                out.printf(" (%.1f%% thread | %.1f%% process)\n", percentThread, percentProcess);
             } else {
                 out.println();
             }
@@ -47,7 +48,7 @@ public class Visualize {
 
         for (CalltreeNode child : children) {
             int nextDepth = skipping && skipped ? depth : depth + 1;
-            print(child, node.getTotalTime(), threadTotalTime, out, nextDepth, config, skipping);
+            print(child, node.getTotalTime(), threadTotalTime, processTotalTime, out, nextDepth, config, skipping);
         }
     }
 
