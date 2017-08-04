714342a8d9fc2ba4295253d908d334abd15b562b
Grigory Turunov <g.turunov@corp.mail.ru>
2017-08-04 18:14:45 +0300

    Add flame graph visualization

--- src/main/java/com/jvmtop/profiler/FlameVisualizer.java
+++ src/main/java/com/jvmtop/profiler/FlameVisualizer.java
@@ -1,9 +1,22 @@
 package com.jvmtop.profiler;
 
 import java.io.PrintStream;
+import java.util.Collection;
 
 public class FlameVisualizer {
     public static void print(CalltreeNode node, PrintStream out) {
-        
+        printInternal("java;", node, out);
+    }
+
+    private static void printInternal(String callstack, CalltreeNode node, PrintStream out) {
+        Collection<CalltreeNode> children = node.getChildren();
+
+        for (CalltreeNode child : children) {
+            printInternal(callstack + node.getName() + ";", child, out);
+        }
+
+        if (node.getSelf() != 0) {
+            out.println(callstack + node.getName() + " " + node.getSelf());
+        }
     }
 }
