3d0156c8479655f3c66b0e14431b8a2ebb0ea4a9
Miroslav Hruz <mhruz@simpleway.cz>
2018-02-07 17:08:50 +0100

    Java 9 support for jvmtop.. finally.
    This new build could connect to Java 9 process or Java 8 and less process in same way as it did in previous versions.

--- src/main/wrappers/jvmtop.bat
+++ src/main/wrappers/jvmtop.bat
@@ -1,4 +1,14 @@
 @echo off
 set DIR=%~dp0
 
-"%JAVA_HOME%\bin\java" %JAVA_OPTS% -cp "%DIR%/jvmtop.jar;%JAVA_HOME%/lib/tools.jar" com.jvmtop.JvmTop %*
+rem In Case of debug, uncomment JAVA_HOME override
+rem set JAVA_HOME="c:\Progra~1\Java\jdk-9.0.1"
+rem set JAVA_HOME="c:\Progra~1\Java\jdk1.8.0_131"
+
+rem To switch from Java 9 to Java 8 comment and uncomment necessary sections
+
+rem java 9 version
+"%JAVA_HOME%\bin\java" %JAVA_OPTS% --add-modules jdk.management,jdk.management.agent,jdk.attach,jdk.internal.jvmstat,java.rmi,java.management.rmi,java.desktop,java.sql --add-exports=java.rmi/sun.rmi.server=ALL-UNNAMED --add-exports=java.rmi/sun.rmi.transport=ALL-UNNAMED --add-exports=jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED -cp "%DIR%/jvmtop.jar" com.jvmtop.JvmTop %*
+
+rem legacy java 8 version
+rem "%JAVA_HOME%\bin\java" %JAVA_OPTS% -cp "%DIR%/jvmtop.jar;%JAVA_HOME%/lib/tools.jar" com.jvmtop.JvmTop %*
--- src/main/wrappers/jvmtop.sh
+++ src/main/wrappers/jvmtop.sh
@@ -1,23 +1,34 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # jvmtop - java monitoring for the command-line 
 # launch script
 #
-# author: Markus Kolb
+# author: Markus Kolb, Miroslav Hruz
 # 
-DIR=$( cd $(dirname $0) ; pwd -P )
+DIR=$(cd $(dirname $0) ; pwd -P)
 
 if [ -z "$JAVA_HOME" ] ; then
-        JAVA_HOME=`readlink -f \`which java 2>/dev/null\` 2>/dev/null | \
-        sed 's/\/bin\/java//'`
+    JAVA_HOME=`readlink -f \`which java 2>/dev/null\` 2>/dev/null | sed 's/\/bin\/java//'`
 fi
 
-TOOLSJAR="$JAVA_HOME/lib/tools.jar"
+JAVA_VERSION=$($JAVA_HOME/bin/java -version 2>&1 | head -n 1 | cut -d " " -f 3 | cut -d "." -f 1 | cut -d "\"" -f 2)
+if [[ $JAVA_VERSION -gt 1 ]]; then
+    ## java 9+
+    "$JAVA_HOME"/bin/java $JAVA_OPTS -cp "$DIR/jvmtop.jar" \
+    	--add-modules jdk.management,jdk.management.agent,jdk.attach,jdk.internal.jvmstat,java.rmi,java.management.rmi,java.desktop,java.sql \
+    	--add-exports=java.rmi/sun.rmi.server=ALL-UNNAMED \
+    	--add-exports=java.rmi/sun.rmi.transport=ALL-UNNAMED \
+    	--add-exports=jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED \
+    	com.jvmtop.JvmTop "$@"
+    exit $?
+else
+    ## legacy java < 9
+    TOOLSJAR="$JAVA_HOME/lib/tools.jar"
 
-if [ ! -f "$TOOLSJAR" ] ; then
-        echo "$JAVA_HOME seems to be no JDK!" >&2
-        exit 1
+    if [ ! -f "$TOOLSJAR" ] ; then
+	    echo "$JAVA_HOME seems to be no JDK!" >&2
+	    exit 1
+    fi
+
+    "$JAVA_HOME"/bin/java $JAVA_OPTS -cp "$DIR/jvmtop.jar":"$TOOLSJAR" com.jvmtop.JvmTop "$@"
+    exit $?
 fi
-
-"$JAVA_HOME"/bin/java $JAVA_OPTS -cp "$DIR/jvmtop.jar:$TOOLSJAR" \
-com.jvmtop.JvmTop "$@"
-exit $?
